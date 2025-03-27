425bb60992021b0ff740f92bc302b25334c5908e
kentcanonigo <canonigorigelkent@gmail.com>
2025-03-28 03:57:15 +0800

    implement env support for custom api url (for docker or local)

--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -1,4 +1,5 @@
 import { Text, View } from 'react-native';
+import ApiTester from './apitester';
 
 export default function Index() {
   return (
@@ -10,6 +11,7 @@ export default function Index() {
       }}
     >
       <Text>Hello world!</Text>
+      <ApiTester />
     </View>
   );
 }
