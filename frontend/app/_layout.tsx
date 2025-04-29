5f893ce45b07776a2ad896bcfa0dbadef7545dd5
Canonigo, Rigel Kent Y. <104291256+kentcanonigo@users.noreply.github.com>
2025-04-29 10:37:25 +0800

    add log statements

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -12,16 +12,21 @@ export default function RootLayout() {
     useEffect(() => {
         // Fake a login check (you'd replace this with real auth logic)
         const checkAuth = async () => {
+            console.log('Checking auth...');
+
             // For example, you could check secure storage here
             // const storedUserType = await SecureStore.getItemAsync('userType');
 
             const storedUserType = null; // simulate not logged in
 
             if (storedUserType === 'admin') {
+                console.log('Logged in as admin');
                 router.replace('/admin' as RelativePathString);
             } else if (storedUserType === 'user') {
+                console.log('Logged in as user');
                 router.replace('/user'as RelativePathString);
             } else {
+                console.log('Not logged in, redirecting to /auth');
                 router.replace('/auth');
             }
 
