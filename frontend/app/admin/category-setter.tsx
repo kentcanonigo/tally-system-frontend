dc2959e6404c5b80c21b46a5220a02c63e0c9c13
KyriosZeroth <131938277+KyriosZeroth@users.noreply.github.com>
2025-04-29 19:12:51 +0800

    Admin Dashboard Created

--- frontend/app/admin/category-setter.tsx
+++ frontend/app/admin/category-setter.tsx
@@ -4,14 +4,43 @@ import ApiTester from "./apitester";
 
 export default function categorySetter() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>You are in the Category Setter page</Text>
+    
+    <View style = {styles.categorysettercontainer}>
+
+       <View style = {styles.Container}>
+         <Text>Create a New Category</Text>
+
+         <Text>Current Product</Text>
+
+         <Text>Category Name</Text>
+
+       </View>
+
+       <View style = {styles.Container}>
+          <Text>You are in the Category Setter page</Text>
+       </View>
+
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  categorysettercontainer:{
+      flex:1,
+      flexDirection:"row",
+      justifyContent:"space-evenly",
+      alignItems:'center',
+      padding: 20,
+  },
+
+  Container:{
+    flex:1,
+    height:'100%',
+    alignItems:"center",
+    justifyContent:"center",
+    backgroundColor:"#F6CEE4",
+    borderRadius:10,
+  },
+
+
+})
